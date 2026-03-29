0326c9d61d45905c1993019451a450955caf66ef
Matias Valmaggia <mvalmaggia@fi.uba.ar>
2026-03-29 20:29:06 -0300

    add basic function to queue

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -6,7 +6,25 @@ from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
-        pass
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=queue_name)
+        self.queue_name = queue_name
+
+    def callback(self, ch, method, properties, body):
+        print(f" [x] Received {body.decode()}")
+        
+    def send(self, message):
+        self.channel.basic_publish(exchange='', routing_key=self.queue_name, body=message)
+
+    def start_consuming(self):
+        self.channel.start_consuming()
+
+    def stop_consuming(self):
+        self.channel.stop_consuming()
+
+    def close(self):
+        self.connection.close()
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
