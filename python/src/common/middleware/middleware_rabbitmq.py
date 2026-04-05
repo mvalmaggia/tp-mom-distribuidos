71325e6836eb83778b8c83fc32a996e2c44abd0b
Matias Valmaggia <mvalmaggia@fi.uba.ar>
2026-04-05 12:50:16 -0300

    add exception handlinh to queue

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -1,7 +1,8 @@
 import pika
-import random
-import string
-from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
+from pika.exceptions import AMQPConnectionError, AMQPChannelError
+
+from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange, MessageMiddlewareDisconnectedError, MessageMiddlewareCloseError, MessageMiddlewareMessageError
+
 
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
@@ -15,12 +16,16 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
         print(f" [x] Received {body.decode()}")
         
     def send(self, message):
-        self.channel.basic_publish(exchange='', routing_key=self.queue_name, body=message)
+        try:
+            self.channel.basic_publish(exchange='', routing_key=self.queue_name, body=message)
+        except (AMQPConnectionError, AMQPChannelError):
+            raise MessageMiddlewareDisconnectedError("Connection lost while sending message.")
+        except Exception as e:
+            raise MessageMiddlewareMessageError(f"An error occurred while sending message: {str(e)}")
+
 
     def start_consuming(self, on_message_callback):
-
         def pika_callback_wrapper(ch, method, properties, body):
-            
             def ack():
                 ch.basic_ack(delivery_tag=method.delivery_tag)
                 
@@ -29,24 +34,49 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
             
             on_message_callback(body, ack, nack)
 
-        self.channel.basic_consume(
-            queue=self.queue_name,
-            on_message_callback=pika_callback_wrapper,
-            auto_ack=False
-        )
+        try:
+            self.channel.basic_consume(
+                queue=self.queue_name,
+                on_message_callback=pika_callback_wrapper,
+                auto_ack=False
+            )
 
-        self.channel.start_consuming()
+            self.channel.start_consuming()
+        except (AMQPConnectionError, AMQPChannelError):
+            raise MessageMiddlewareDisconnectedError("Connection lost while consuming messages.")
+        except Exception as e:
+            raise MessageMiddlewareMessageError(f"An error occurred while consuming messages: {str(e)}")
 
     def stop_consuming(self):
-        self.channel.stop_consuming()
+        try:
+            self.channel.stop_consuming()
+        except (AMQPConnectionError, AMQPChannelError):             
+            raise MessageMiddlewareDisconnectedError("Connection lost while stopping consumption.")
+        except Exception:
+            pass
 
     def close(self):
-        self.connection.close()
+        try:
+            if self.connection.is_open:
+                self.connection.close()
+        except:
+            raise MessageMiddlewareCloseError("Failed to close the connection properly.")
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
     def __init__(self, host, exchange_name, routing_keys):
-        pass
+        self._exhange_name = exchange_name
+        self._routing_keys = routing_keys
+        self._bind_to_queues(routing_keys)
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange=exchange_name, exchange_type='direct')
+
+    def _bind_to_queues(self, routing_keys):
+        for routing_key in routing_keys:
+            queue_name = f"{self._exhange_name}_{routing_key}"
+            self.channel.queue_declare(queue=queue_name)
+            self.channel.queue_bind(exchange=self._exhange_name, queue=queue_name, routing_key=routing_key)
 
     def send(self, message):
         pass
